ac210e67f60c97645f87fb25ee00cf50443f48ba
신혜민 <alddlv81@gmail.com>
2024-09-09 15:30:18 +0900

    Feat-FE: 거울모드 화면 생성 및 1초마다 visable 변경 (#1)

---
+++ frontend/SmartMirror/MirrorOutputForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SmartMirror
+{
+    partial class MirrorOutputForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MirrorOutputForm));
+            pictureBox1 = new PictureBox();
+            title = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Bottom;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(476, 1367);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(676, 1120);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // title
+            // 
+            title.Anchor = AnchorStyles.Top;
+            title.Font = new Font("맑은 고딕", 30F);
+            title.ForeColor = SystemColors.ControlLightLight;
+            title.Location = new Point(-3, 9);
+            title.Name = "title";
+            title.Size = new Size(1576, 136);
+            title.TabIndex = 1;
+            title.Text = "찾으시는 제품 있으면 말씀해 주세요";
+            title.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // MirrorOutputForm
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1574, 2489);
+            Controls.Add(title);
+            Controls.Add(pictureBox1);
+            Name = "MirrorOutputForm";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            Load += MirrorOutputForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label title;
+    }
+}
