171b11d6312f0c13c39bb1cbf5526b0ddd9f83fc
신혜민 <alddlv81@gmail.com>
2024-09-10 15:06:57 +0900

    Feat-Fe: 폼 2개 화면 시작 위치 변경 (#1)

--- frontend/SmartMirror/MirrorInputForm.cs
+++ frontend/SmartMirror/MirrorInputForm.cs
@@ -1,27 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace SmartMirror
+﻿namespace SmartMirror
 {
     public partial class MirrorInputForm : Form
     {
-        public MirrorInputForm()
+        private MirrorOutputForm mirrorOutputForm;
+        private int monitorIndex = 1;
+
+        public MirrorInputForm(MirrorOutputForm outputForm)
         {
             InitializeComponent();
-            //this.WindowState = FormWindowState.Maximized;
-            //this.FormBorderStyle = FormBorderStyle.None;
+            this.mirrorOutputForm = outputForm;
         }
 
         private void MirrorInputForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 현재 MirrorInputForm을 숨김
+            this.Hide();
+
+            // MainInputForm을 생성하고 표시
+            MainInputForm mainInputForm = new MainInputForm();
+
+            // 메인 인풋 폼을 특정 모니터에 표시 (예: 첫 번째 모니터)
+            Screen primaryScreen = Screen.AllScreens[0];
+            mainInputForm.StartPosition = FormStartPosition.Manual;
+            mainInputForm.Location = primaryScreen.Bounds.Location;
+            //mainInputForm.Size = new Size(primaryScreen.Bounds.Width, primaryScreen.Bounds.Height);
+            mainInputForm.Show();
+
+            // MirrorOutputForm을 MainOutputForm으로 변경
+            MainOutputForm mainOutputForm = new MainOutputForm();
+
+            // MainOutputForm을 두 번째 모니터에 표시
+            Screen secondaryScreen = Screen.AllScreens[monitorIndex];
+            mainOutputForm.StartPosition = FormStartPosition.Manual;
+            mainOutputForm.Location = secondaryScreen.Bounds.Location;
+            mainOutputForm.Size = new Size(secondaryScreen.Bounds.Width, secondaryScreen.Bounds.Height);
+
+            mirrorOutputForm.Hide(); // MirrorOutputForm을 숨기고
+            mainOutputForm.Show();   // MainOutputForm을 표시
         }
     }
 }
