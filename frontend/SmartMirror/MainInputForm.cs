20c6537dba6cd8b557e0df9fd566725e76f4cbd3
신혜민 <alddlv81@gmail.com>
2024-09-10 15:05:18 +0900

    Design-FE: 메인 화면 input, output 디자인

---
+++ frontend/SmartMirror/MainInputForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartMirror
+{
+    public partial class MainInputForm : Form
+    {
+        public MainInputForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainInputForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
