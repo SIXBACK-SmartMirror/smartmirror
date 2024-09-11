3b84461eba30908613f16bc7e9993037522c680d
김아름 <rdf136@naver.com>
2024-09-12 02:50:06 +0900

    Feat-BE: 상품 위치 칼럼 Json 구조 Dto 구현 (#4)
    Json 구조
    {
    "name" : ,
    "row" : ,
    "col" :
    }

---
+++ backend/src/main/java/com/sixback/backend/domain/dto/LocationDto.java
@@ -0,0 +1,24 @@
+package com.sixback.backend.domain.dto;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LocationDto {
+    private String name;
+    private short row;
+    private short col;
+
+    @Override
+    public String toString() {
+        return String.format("""
+                {
+                \"name\":\"%s\",
+                \"row\":%d,
+                \"col\":%d,
+                }
+                """, name, row, col);
+    }
+}
