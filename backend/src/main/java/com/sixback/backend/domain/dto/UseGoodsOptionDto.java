1d22b3824268e01cbb01e0020a0320c215f2d03d
김아름 <rdf136@naver.com>
2024-09-12 02:47:14 +0900

    Feat-BE: style 테이블 goodsOptionList 칼럼 Json 구조 Dto 구현 (#4)
    Json 구조
    {
    "option_id" : ,
    "goods_name" : ,
    "option_name" : ,
    "option_image" :
    }

---
+++ backend/src/main/java/com/sixback/backend/domain/dto/UseGoodsOptionDto.java
@@ -0,0 +1,28 @@
+package com.sixback.backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UseGoodsOptionDto {
+    private Long optionId;
+    private String goodsName;
+    private String optionName;
+    private String optionImage;
+
+    @Override
+    public String toString() {
+        return String.format("""
+                {
+                \"option_id\":%d,
+                \"goods_name\":\"%s\",
+                \"option_name\":\"%s\",
+                \"option_image\":\"%s\",
+                }
+                """, optionId, goodsName, optionName, optionImage);
+    }
+}
