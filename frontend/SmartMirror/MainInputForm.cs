a8bbc773d8cc747e8bff8bbd200873857af70d69
신혜민 <alddlv81@gmail.com>
2024-09-12 10:18:23 +0900

    Refactor-FE: 중복 코드 삭제 (#3)

--- frontend/SmartMirror/MainInputForm.cs
+++ frontend/SmartMirror/MainInputForm.cs
@@ -46,7 +46,7 @@ namespace SmartMirror
             searchOutputForm.Show();
         }
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
+        private void panel_Paint(object sender, PaintEventArgs e)
         {
             // 둥근 모서리 반지름 설정
             int cornerRadius = 15;
@@ -64,49 +64,9 @@ namespace SmartMirror
             path.CloseFigure();
 
             // 패널의 모양을 둥근 모서리로 설정
-            panel2.Region = new Region(path);
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-            // 둥근 모서리 반지름 설정
-            int cornerRadius = 15;
-
-            // 패널의 크기
-            int panelWidth = panel3.Width;
-            int panelHeight = panel3.Height;
-
-            // GraphicsPath를 사용해 둥근 모서리 경로를 생성
-            GraphicsPath path = new GraphicsPath();
-            path.AddArc(new Rectangle(0, 0, cornerRadius, cornerRadius), 180, 90);  // 좌상단
-            path.AddArc(new Rectangle(panelWidth - cornerRadius, 0, cornerRadius, cornerRadius), 270, 90); // 우상단
-            path.AddArc(new Rectangle(panelWidth - cornerRadius, panelHeight - cornerRadius, cornerRadius, cornerRadius), 0, 90); // 우하단
-            path.AddArc(new Rectangle(0, panelHeight - cornerRadius, cornerRadius, cornerRadius), 90, 90); // 좌하단
-            path.CloseFigure();
-
-            // 패널의 모양을 둥근 모서리로 설정
-            panel3.Region = new Region(path);
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            // 둥근 모서리 반지름 설정
-            int cornerRadius = 15;
-
-            // 패널의 크기
-            int panelWidth = panel1.Width;
-            int panelHeight = panel1.Height;
-
-            // GraphicsPath를 사용해 둥근 모서리 경로를 생성
-            GraphicsPath path = new GraphicsPath();
-            path.AddArc(new Rectangle(0, 0, cornerRadius, cornerRadius), 180, 90);  // 좌상단
-            path.AddArc(new Rectangle(panelWidth - cornerRadius, 0, cornerRadius, cornerRadius), 270, 90); // 우상단
-            path.AddArc(new Rectangle(panelWidth - cornerRadius, panelHeight - cornerRadius, cornerRadius, cornerRadius), 0, 90); // 우하단
-            path.AddArc(new Rectangle(0, panelHeight - cornerRadius, cornerRadius, cornerRadius), 90, 90); // 좌하단
-            path.CloseFigure();
-
-            // 패널의 모양을 둥근 모서리로 설정
             panel1.Region = new Region(path);
+            panel2.Region = new Region(path);
+            panel3.Region = new Region(path);
         }
     }
 }
