52dbd06e78c39f73660f6f127a7900f04b5455aa
Kim Areum <areumk163@gmail.com>
2024-09-20 02:48:24 +0900

    Feat-BE: 빈 파일이 전달될 때 예외 처리 (#16)
    Former-commit-id: debddbe1c27d79f0345a2f0d168ded8ccbda0b8a

---
+++ backend/src/main/java/com/sixback/backend/common/exception/EmptyFileException.java
@@ -0,0 +1,10 @@
+package com.sixback.backend.common.exception;
+
+/**
+ * 빈 파일이 전달됐을 떄 예외 발생
+ */
+public class EmptyFileException extends CustomFileException {
+	public EmptyFileException() {
+		super("F02");
+	}
+}
