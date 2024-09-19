1c8996c05aa33925c4b87e6309ad2879e4e0477e
김아름 <rdf136@naver.com>
2024-09-19 21:03:36 +0900

    Feat-BE: style 리스트 API 컨트롤러 구현 (#9)

--- backend/src/main/java/com/sixback/backend/domain/controller/StyleController.java
+++ backend/src/main/java/com/sixback/backend/domain/controller/StyleController.java
@@ -1,23 +1,27 @@
 package com.sixback.backend.domain.controller;
 
 import java.util.ArrayList;
-import java.util.Base64;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sixback.backend.common.dto.ResponseDto;
+import com.sixback.backend.domain.dto.StyleInfoDto;
 import com.sixback.backend.domain.dto.StyleResultDto;
 import com.sixback.backend.domain.dto.VirtualMakeupReqDto;
 import com.sixback.backend.domain.service.StyleService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -29,6 +33,15 @@ import reactor.core.publisher.Mono;
 public class StyleController {
 
 	private final StyleService styleService;
+
+	// 화장 스타일 목록 조회
+	@GetMapping
+	public ResponseEntity<?> findAllStyle(@PathVariable("marketId") Long marketId,
+		@Min(0) @RequestParam("page") int page,
+		@Min(1) @RequestParam("size") int size) {
+		Page<StyleInfoDto> styleInfoDtoPage = styleService.findAllStyle(marketId, page, size);
+		return new ResponseEntity<>(new ResponseDto<>("A00", styleInfoDtoPage), HttpStatus.OK);
+	}
 
 	// 가상 화장 하기
 	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
