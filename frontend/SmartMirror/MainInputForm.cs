c693262bad7f7fe7a2658be7b07434c500d2b08b
kimjongdeok <whd4638@naver.com>
2024-09-19 10:56:34 +0900

    Feat-FE: 사진찎기,저장하기 기능 개발(#2)

---
+++ frontend/SmartMirror/MainInputForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartMirror
+{
+    public partial class MainInputForm : Form
+    {
+        public MainInputForm()
+        {
+            InitializeComponent();
+        }
+
+        private void makeup_Click(object sender, EventArgs e)
+        {
+            
+            int monitorIndex = 1;
+            MakeupOutputForm outputForm = new MakeupOutputForm();
+
+            Screen mirror = Screen.AllScreens[monitorIndex];
+
+            outputForm.StartPosition = FormStartPosition.Manual;
+            outputForm.Location = mirror.Bounds.Location;
+
+            // MakeupInform show
+            Console.WriteLine("연결");
+            outputForm.Show();
+
+            // MaininputForm 숨기기
+            this.Hide();
+
+            MakeupInputForm inputForm = new MakeupInputForm(outputForm);
+            //inputForm.Owner = this;
+            inputForm.Show();
+            
+        }
+
+        private void MainInputForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ frontend/SmartMirror/MakeupInputForm.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SmartMirror
+{
+    public partial class MakeupInputForm : Form
+    {
+        private MakeupOutputForm outputForm;
+        private StyleInputForm styleInputForm;
+
+        public MakeupInputForm(MakeupOutputForm outputForm)
+        {
+            this.outputForm = outputForm;
+            InitializeComponent();
+        }
+
+        private void filmingBtn_Click(object sender, EventArgs e)
+        {
+            outputForm.CaptureImage();
+        }
+
+        private void MakeupInputForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void usingBtn_Click(object sender, EventArgs e)
+        {
+            StyleInputForm styleInputForm = new StyleInputForm();
+            this.Hide();
+            styleInputForm.Show();
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            // 둥근 모서리 반지름 설정
+            int cornerRadius = 15;
+
+            // 패널의 크기
+            int panelWidth = HomeBtn.Width;
+            int panelHeight = HomeBtn.Height;
+
+            // GraphicsPath를 사용해 둥근 모서리 경로를 생성
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(new Rectangle(0, 0, cornerRadius, cornerRadius), 180, 90);  // 좌상단
+            path.AddArc(new Rectangle(panelWidth - cornerRadius, 0, cornerRadius, cornerRadius), 270, 90); // 우상단
+            path.AddArc(new Rectangle(panelWidth - cornerRadius, panelHeight - cornerRadius, cornerRadius, cornerRadius), 0, 90); // 우하단
+            path.AddArc(new Rectangle(0, panelHeight - cornerRadius, cornerRadius, cornerRadius), 90, 90); // 좌하단
+            path.CloseFigure();
+
+            // 패널의 모양을 둥근 모서리로 설정
+            HomeBtn.Region = new Region(path);
+            filmingBtn.Region = new Region(path);
+            usingBtn.Region = new Region(path);
+        }
+
+
+    }
+}
