af9b2043ff1bf54511fc8d870acd1a9f35410084
신혜민 <alddlv81@gmail.com>
2024-09-20 09:36:28 +0900

    Fix-FE: 스크린 2개일때, input창 0에 표시 (#15)
    Former-commit-id: 288902520f275fbd2d8ddd02a22fb6f98d23470e

--- frontend/SmartMirror/SearchInfoInputForm.Designer.cs
+++ frontend/SmartMirror/SearchInfoInputForm.Designer.cs
@@ -28,12 +28,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SearchInfoInputForm";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchInfoInputForm));
+            panel5 = new Panel();
+            panel6 = new Panel();
+            pictureBox3 = new PictureBox();
+            label6 = new Label();
+            label4 = new Label();
+            panel4 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
+            panel11 = new Panel();
+            panel12 = new Panel();
+            panel13 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel7.SuspendLayout();
+            panel8.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel5
+            // 
+            panel5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel5.BackColor = Color.FromArgb(130, 220, 40);
+            panel5.Location = new Point(-2, -2);
+            panel5.Margin = new Padding(2);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(99, 1015);
+            panel5.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            panel6.Anchor = AnchorStyles.None;
+            panel6.AutoScroll = true;
+            panel6.BackColor = Color.White;
+            panel6.Controls.Add(panel13);
+            panel6.Controls.Add(panel12);
+            panel6.Controls.Add(panel11);
+            panel6.Controls.Add(panel10);
+            panel6.Controls.Add(panel9);
+            panel6.Controls.Add(panel3);
+            panel6.Controls.Add(panel2);
+            panel6.Controls.Add(panel1);
+            panel6.Location = new Point(100, 148);
+            panel6.Margin = new Padding(2);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(1369, 848);
+            panel6.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Anchor = AnchorStyles.Left;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(19, 11);
+            pictureBox3.Margin = new Padding(2);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(53, 53);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 1;
+            pictureBox3.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Left;
+            label6.AutoSize = true;
+            label6.Font = new Font("맑은 고딕", 20F, FontStyle.Bold);
+            label6.Location = new Point(82, 11);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(140, 54);
+            label6.TabIndex = 0;
+            label6.Text = "총 0개";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("맑은 고딕", 15F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(255, 120, 120);
+            label4.Location = new Point(1250, 17);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(209, 41);
+            label4.TabIndex = 2;
+            label4.Text = "올브영 구미점";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(255, 120, 120);
+            panel4.Location = new Point(13, 4);
+            panel4.Margin = new Padding(2);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(13, 18);
+            panel4.TabIndex = 4;
+            // 
+            // panel7
+            // 
+            panel7.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            panel7.BackColor = Color.FromArgb(130, 220, 40);
+            panel7.Controls.Add(panel4);
+            panel7.Location = new Point(1217, 21);
+            panel7.Margin = new Padding(2);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(32, 35);
+            panel7.TabIndex = 7;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.White;
+            panel8.Controls.Add(label6);
+            panel8.Controls.Add(pictureBox3);
+            panel8.Location = new Point(97, 69);
+            panel8.Margin = new Padding(2);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(760, 75);
+            panel8.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(49, 33);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(309, 342);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(364, 33);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(309, 342);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(679, 33);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(309, 342);
+            panel3.TabIndex = 1;
+            // 
+            // panel9
+            // 
+            panel9.Location = new Point(993, 33);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(309, 342);
+            panel9.TabIndex = 1;
+            // 
+            // panel10
+            // 
+            panel10.Location = new Point(49, 403);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(309, 342);
+            panel10.TabIndex = 1;
+            // 
+            // panel11
+            // 
+            panel11.Location = new Point(364, 403);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(309, 342);
+            panel11.TabIndex = 1;
+            // 
+            // panel12
+            // 
+            panel12.Location = new Point(679, 403);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(309, 342);
+            panel12.TabIndex = 1;
+            // 
+            // panel13
+            // 
+            panel13.Location = new Point(994, 403);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(309, 342);
+            panel13.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(15, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(279, 246);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // SearchInfoInputForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(1470, 1000);
+            Controls.Add(panel8);
+            Controls.Add(panel7);
+            Controls.Add(label4);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2);
+            Name = "SearchInfoInputForm";
+            Text = "Form1";
+            panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel7.ResumeLayout(false);
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Panel panel5;
+        private Panel panel6;
+        private Label label6;
+        private PictureBox pictureBox3;
+        private Label label4;
+        private Panel panel4;
+        private Panel panel7;
+        private Panel panel8;
+        private Panel panel1;
+        private Panel panel13;
+        private Panel panel12;
+        private Panel panel11;
+        private Panel panel10;
+        private Panel panel9;
+        private Panel panel3;
+        private Panel panel2;
+        private PictureBox pictureBox1;
     }
 }
