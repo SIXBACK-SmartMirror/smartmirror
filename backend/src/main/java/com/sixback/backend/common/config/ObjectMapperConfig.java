c77a31b07bd42683a2d4715927a0a4cefa22e75e
Kim Areum <areumk163@gmail.com>
2024-09-20 10:48:01 +0900

    Fix-BE: ObjectMapperConfig 수정 (#17)
    Jackson2ObjectMapperBuilder를 사용하면 Spring의 기본 ObjectMapper 설정 유지
    
    
    Former-commit-id: 5c49825ef3e9ad077ce3148f3850e3a44df40182

--- backend/src/main/java/com/sixback/backend/common/config/ObjectMapperConfig.java
+++ backend/src/main/java/com/sixback/backend/common/config/ObjectMapperConfig.java
@@ -2,13 +2,16 @@ package com.sixback.backend.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ObjectMapperConfig {
 	@Bean
-	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
+	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+		return builder.createXmlMapper(false).build();
 	}
 }
