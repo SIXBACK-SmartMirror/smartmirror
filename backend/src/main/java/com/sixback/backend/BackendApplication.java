a8a111d2200d8a74e333725871d4d1491ab864e2
김아름 <rdf136@naver.com>
2024-09-20 10:48:01 +0900

    Fix-BE: ObjectMapperConfig 수정 (#17)
    Jackson2ObjectMapperBuilder를 사용하면 Spring의 기본 ObjectMapper 설정 유지
    
    
    Former-commit-id: 111c7e7c16f597a854f7ed85f3be208ead7db782

--- backend/src/main/java/com/sixback/backend/BackendApplication.java
+++ backend/src/main/java/com/sixback/backend/BackendApplication.java
@@ -2,10 +2,8 @@ package com.sixback.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BackendApplication {
 
 	public static void main(String[] args) {
