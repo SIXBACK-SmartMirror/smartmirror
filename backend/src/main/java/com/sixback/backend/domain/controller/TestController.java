6a8b9d9db7c4d54265fb86af3787cac684b21f3a
김찬민 <thesimpleman@naver.com>
2024-09-23 22:25:53 +0900

    Move-CICD: backend TestController 위치 변경 (#19)
    Former-commit-id: c676530d2e1b0e4ff0f098204ec976287434d26b

--- backend/src/main/java/com/sixback/backend/domain/controller/TestController.java
+++ backend/src/main/java/com/sixback/backend/domain/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.sixback.backend;
+package com.sixback.backend.domain.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
