b7ff920e50c1dba7144e1d3e9d6f11ce6b8f4e27
김아름 <rdf136@naver.com>
2024-09-23 17:35:33 +0900

    Feat-BE: styleId - optionId 검증 쿼리 추가 (#10)
    Former-commit-id: 48f08c74af4549a790b9961da9f03e66be300370

--- backend/src/main/java/com/sixback/backend/domain/repository/StyleRepository.java
+++ backend/src/main/java/com/sixback/backend/domain/repository/StyleRepository.java
@@ -1,10 +1,15 @@
 package com.sixback.backend.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.sixback.backend.domain.dto.OptionInfoDto;
 import com.sixback.backend.domain.dto.StyleInfoDto;
 import com.sixback.backend.domain.entity.Style;
 
@@ -34,4 +39,11 @@ public interface StyleRepository extends JpaRepository<Style, Long> {
 		""", nativeQuery = true)
 	List<OptionInfoDto> findAllUseOptionLocationList(@Param("marketId") Long marketId, @Param("styleId") Long styleId);
 
+	@Query(value = """
+			SELECT *
+			FROM Style s
+			WHERE s.style_id = :styleId
+			AND JSON_CONTAINS(s.goods_option_list, CONCAT('{"option_id":', :optionId, '}')) = TRUE
+		""", nativeQuery = true)
+	Optional<Style> findByStyleIdAndOptionId(Long styleId, Long optionId);
 }
