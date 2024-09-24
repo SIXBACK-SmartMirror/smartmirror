aa477fbe3d8b633d8c0af7305302cd836ccf8dac
김찬민 <thesimpleman@naver.com>
2024-09-24 13:37:57 +0900

    Fix-BE: backend 누락된 참조 추가 (#10)
    Former-commit-id: a992d6b5bb14f695f899371b5d7587a657e814f5

--- backend/src/main/java/com/sixback/backend/domain/repository/GoodsOptionRepository.java
+++ backend/src/main/java/com/sixback/backend/domain/repository/GoodsOptionRepository.java
@@ -2,6 +2,7 @@ package com.sixback.backend.domain.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
