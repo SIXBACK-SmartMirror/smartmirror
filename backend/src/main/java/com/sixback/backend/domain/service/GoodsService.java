4a9dad4b4a68898e968785ddbc02694a84c6a56f
Kim Areum <areumk163@gmail.com>
2024-09-24 09:07:15 +0900

    Feat-BE: 키워드 기반 검색 findByGoodsId 처리 서비스 틀 구현 (#11)
    Former-commit-id: 5c2b6e4c925b866abd7978d468436fdf5396a76f

--- backend/src/main/java/com/sixback/backend/domain/service/GoodsService.java
+++ backend/src/main/java/com/sixback/backend/domain/service/GoodsService.java
@@ -1,12 +1,18 @@
 package com.sixback.backend.domain.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.sixback.backend.common.exception.GoodsNotFoundException;
+import com.sixback.backend.domain.dto.GoodsDetailDto;
 import com.sixback.backend.domain.dto.GoodsDto;
+import com.sixback.backend.domain.dto.OptionDto;
+import com.sixback.backend.domain.dto.OptionInfoDto;
 import com.sixback.backend.domain.dto.SearchResultDto;
 import com.sixback.backend.domain.repository.GoodsOptionRepository;
 
@@ -27,4 +33,15 @@ public class GoodsService {
 		Page<GoodsDto> goodsDtoPage = goodsOptionRepository.findAllGoodsByKeyword(keyword, pageable);
 		return new SearchResultDto(keyword, goodsDtoPage);
 	}
+
+	public GoodsDetailDto findByGoodsId(Long marketId, Long goodsId) {
+		// 매장 유효성 검사
+		marketService.validateMarket(marketId);
+		// goods_id 검사
+		goodsOptionRepository.findByValidGoodsId(goodsId).orElseThrow(GoodsNotFoundException::new);
+		List<OptionDto> optionDtoList = null;
+		List<OptionInfoDto> locationList = null;
+		return new GoodsDetailDto(optionDtoList, locationList);
+	}
+
 }
