2215e33ae9e887131bcfae0bfee58060a60e9c37
김아름 <rdf136@naver.com>
2024-09-25 16:16:00 +0900

    Feat-BE: 외부 API 환경 변수 적용 (#23)
    Former-commit-id: 6a022fedc400ae6f90a29cdd6498dbbfeb4d9d9b

--- backend/src/main/java/com/sixback/backend/common/config/WebClientConfig.java
+++ backend/src/main/java/com/sixback/backend/common/config/WebClientConfig.java
@@ -1,12 +1,24 @@
 package com.sixback.backend.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+	@Value("${spring.data.api.key}")
+	private String API_KEY;
+	@Value("${spring.data.api.url}")
+	private String API_URL;
+	@Value("$spring.data.chat.url}")
+	private String CHAT_API_URL;
+	@Value("$spring.data.gan.url}")
+	private String GAN_API_URL;
 
 	// Base configuration for WebClient
 	@Bean
@@ -22,7 +34,7 @@ public class WebClientConfig {
 	@Bean
 	public WebClient ganWebClient(WebClient.Builder webClientBuilder) {
 		return webClientBuilder
-			.baseUrl("http://localhost:8000")
+			.baseUrl(GAN_API_URL)
 			.build();
 	}
 
@@ -30,8 +42,9 @@ public class WebClientConfig {
 	@Bean
 	public WebClient sttWebClient(WebClient.Builder webClientBuilder) {
 		return webClientBuilder
-			.baseUrl("https://stt-server-url.com")
-			.defaultHeader("Content-Type", "multipart/form-data")  // For sending audio files
+			.baseUrl(API_URL)
+			.defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY)
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
 			.build();
 	}
 
@@ -39,8 +52,9 @@ public class WebClientConfig {
 	@Bean
 	public WebClient nlpWebClient(WebClient.Builder webClientBuilder) {
 		return webClientBuilder
-			.baseUrl("https://nlp-server-url.com")
-			.defaultHeader("Content-Type", "application/json")  // Assuming NLP server expects JSON data
+			.baseUrl(API_URL)
+			.defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + API_KEY)
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
 			.build();
 	}
 }
