4e82f9c827ce3a7290cb7eea879344e4e2c0ff48
Kim Areum <areumk163@gmail.com>
2024-09-25 23:34:38 +0900

    Feat-BE: 상품 추출 요청 DTO 구현 (#23)
    Former-commit-id: ee17475c724ee3bfa54c760831e16281cb7019b0

---
+++ backend/src/main/java/com/sixback/backend/common/dto/nlp/NLPProductExtractionDto.java
@@ -0,0 +1,39 @@
+package com.sixback.backend.common.dto.nlp;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NLPProductExtractionDto {
+	//사용할 모델의 이름
+	private String model;
+	// 전달할 메시지 목록(ChatMessageDto 객체로 표현)
+	private List<ChatMessageDto> messages;
+	// 생성되는 응답의 다양성을 조절하는 온도 값(기본값은 0.8)
+	private float temperature = 0.8f;
+	// 생성할 수 있는 최대 토큰 수(기본값은 35)
+	private short max_completion_tokens= 120;
+
+	/**
+	 * 객체를 JSON 문자열로 변환하여 반환.
+	 *
+	 * @return 변환된 JSON 형태의 문자열
+	 */
+	@Override
+	public String toString() {
+		return new StringBuilder()
+			.append("{\"model\":\"").append(model)
+			.append("\", \"messages\":").append(messages)
+			.append(", \"temperature\":").append(temperature)
+			.append(", \"max_tokens\":").append(max_completion_tokens)
+			.append("}").toString();
+	}
+}
