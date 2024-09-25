28b5f364d02f67070548ed15a44f714419cce498
Kim Areum <areumk163@gmail.com>
2024-09-25 16:13:10 +0900

    Feat-BE: 음성 검색 API controller 추가 (#23)
    Former-commit-id: 652cb0c50ee42940f257c6be83e19a51c1ad57c0

--- backend/src/main/java/com/sixback/backend/domain/controller/GoodsController.java
+++ backend/src/main/java/com/sixback/backend/domain/controller/GoodsController.java
@@ -1,18 +1,22 @@
 package com.sixback.backend.domain.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sixback.backend.common.dto.ResponseDto;
 import com.sixback.backend.domain.dto.GoodsDetailDto;
+import com.sixback.backend.domain.dto.SearchReqDto;
 import com.sixback.backend.domain.dto.SearchResultDto;
 import com.sixback.backend.domain.service.GoodsService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,6 +25,14 @@ import lombok.RequiredArgsConstructor;
 public class GoodsController {
 
 	private final GoodsService goodsService;
+
+	//음성 검색
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<?> findAllGoodsBySTT(@PathVariable("marketId") Long marketId,
+		@Valid @ModelAttribute SearchReqDto searchReqDto) {
+		SearchResultDto searchResultDto = goodsService.findAllGoods(marketId, searchReqDto);
+		return new ResponseEntity<>(new ResponseDto<>("A00", searchResultDto), HttpStatus.OK);
+	}
 
 	// 타이핑 검색
 	@GetMapping
