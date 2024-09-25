e0f5c2a331dc0029e835ba5ef17522c9f77f3843
신혜민 <alddlv81@gmail.com>
2024-09-25 15:55:11 +0900

    Feat-FE: 상품 상세보기 클릭시 기존 데이터 띄우기 (#20)
    Former-commit-id: 95889414351cde04d6b092018d13a568044a7f88

---
+++ frontend/SmartMirror/SearchDetailInputForm.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace SmartMirror
+{
+    partial class SearchDetailInputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel5 = new Panel();
+            panel16 = new Panel();
+            label9 = new Label();
+            panel14 = new Panel();
+            label6 = new Label();
+            panel15 = new Panel();
+            label8 = new Label();
+            label4 = new Label();
+            panel4 = new Panel();
+            panel7 = new Panel();
+            panel6 = new Panel();
+            panel1 = new Panel();
+            label11 = new Label();
+            label10 = new Label();
+            label7 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            img = new PictureBox();
+            panel8 = new Panel();
+            comboBox1 = new ComboBox();
+            discountPrice = new Label();
+            price = new Label();
+            name = new Label();
+            brand = new Label();
+            panel5.SuspendLayout();
+            panel16.SuspendLayout();
+            panel14.SuspendLayout();
+            panel15.SuspendLayout();
+            panel7.SuspendLayout();
+            panel6.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)img).BeginInit();
+            panel8.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel5
+            // 
+            panel5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel5.BackColor = Color.FromArgb(130, 220, 40);
+            panel5.Controls.Add(panel16);
+            panel5.Controls.Add(panel14);
+            panel5.Controls.Add(panel15);
+            panel5.Location = new Point(-2, -2);
+            panel5.Margin = new Padding(2);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(99, 1015);
+            panel5.TabIndex = 5;
+            // 
+            // panel16
+            // 
+            panel16.BackColor = Color.FromArgb(224, 224, 224);
+            panel16.Controls.Add(label9);
+            panel16.Location = new Point(2, 371);
+            panel16.Name = "panel16";
+            panel16.Size = new Size(100, 100);
+            panel16.TabIndex = 9;
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.None;
+            label9.AutoSize = true;
+            label9.Font = new Font("맑은 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label9.ForeColor = Color.Black;
+            label9.Location = new Point(33, 34);
+            label9.Name = "label9";
+            label9.Size = new Size(32, 28);
+            label9.TabIndex = 0;
+            label9.Text = "홈";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel14
+            // 
+            panel14.BackColor = Color.FromArgb(224, 224, 224);
+            panel14.Controls.Add(label6);
+            panel14.Location = new Point(0, 271);
+            panel14.Name = "panel14";
+            panel14.Size = new Size(100, 100);
+            panel14.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.None;
+            label6.AutoSize = true;
+            label6.Font = new Font("맑은 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(13, 35);
+            label6.Name = "label6";
+            label6.Size = new Size(72, 28);
+            label6.TabIndex = 0;
+            label6.Text = "재검색";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel15
+            // 
+            panel15.BackColor = Color.White;
+            panel15.Controls.Add(label8);
+            panel15.Location = new Point(0, 171);
+            panel15.Name = "panel15";
+            panel15.Size = new Size(100, 100);
+            panel15.TabIndex = 9;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.None;
+            label8.AutoSize = true;
+            label8.Font = new Font("맑은 고딕", 10F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label8.Location = new Point(24, 37);
+            label8.Name = "label8";
+            label8.Size = new Size(52, 28);
+            label8.TabIndex = 0;
+            label8.Text = "결과";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("맑은 고딕", 15F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(255, 120, 120);
+            label4.Location = new Point(1250, 17);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(209, 41);
+            label4.TabIndex = 2;
+            label4.Text = "올브영 구미점";
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(255, 120, 120);
+            panel4.Location = new Point(13, 4);
+            panel4.Margin = new Padding(2);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(13, 18);
+            panel4.TabIndex = 4;
+            // 
+            // panel7
+            // 
+            panel7.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            panel7.BackColor = Color.FromArgb(130, 220, 40);
+            panel7.Controls.Add(panel4);
+            panel7.Location = new Point(1217, 21);
+            panel7.Margin = new Padding(2);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(32, 35);
+            panel7.TabIndex = 7;
+            // 
+            // panel6
+            // 
+            panel6.Anchor = AnchorStyles.None;
+            panel6.AutoScroll = true;
+            panel6.BackColor = Color.White;
+            panel6.Controls.Add(panel1);
+            panel6.Controls.Add(panel3);
+            panel6.Location = new Point(100, 80);
+            panel6.Margin = new Padding(2);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(1366, 916);
+            panel6.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(436, 433);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(878, 131);
+            panel1.TabIndex = 2;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("맑은 고딕", 11F, FontStyle.Bold);
+            label11.Location = new Point(694, 51);
+            label11.Name = "label11";
+            label11.Size = new Size(0, 30);
+            label11.TabIndex = 6;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("맑은 고딕", 11F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label10.ForeColor = Color.FromArgb(130, 220, 40);
+            label10.Location = new Point(466, 51);
+            label10.Name = "label10";
+            label10.Size = new Size(0, 30);
+            label10.TabIndex = 5;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("맑은 고딕", 11F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label7.Location = new Point(135, 51);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 30);
+            label7.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(39, 36);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(67, 63);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(img);
+            panel3.Controls.Add(panel8);
+            panel3.Location = new Point(53, 23);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1261, 387);
+            panel3.TabIndex = 3;
+            // 
+            // img
+            // 
+            img.Location = new Point(20, 20);
+            img.Name = "img";
+            img.Size = new Size(348, 346);
+            img.SizeMode = PictureBoxSizeMode.StretchImage;
+            img.TabIndex = 0;
+            img.TabStop = false;
+            // 
+            // panel8
+            // 
+            panel8.Controls.Add(comboBox1);
+            panel8.Controls.Add(discountPrice);
+            panel8.Controls.Add(price);
+            panel8.Controls.Add(name);
+            panel8.Controls.Add(brand);
+            panel8.Location = new Point(392, 20);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(845, 346);
+            panel8.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(30, 237);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(484, 40);
+            comboBox1.TabIndex = 4;
+            comboBox1.Text = "상품을 선택해주세요";
+            // 
+            // discountPrice
+            // 
+            discountPrice.AutoSize = true;
+            discountPrice.Font = new Font("맑은 고딕", 16F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            discountPrice.ForeColor = Color.FromArgb(236, 0, 6);
+            discountPrice.Location = new Point(144, 146);
+            discountPrice.Name = "discountPrice";
+            discountPrice.Size = new Size(0, 45);
+            discountPrice.TabIndex = 3;
+            // 
+            // price
+            // 
+            price.AutoSize = true;
+            price.Font = new Font("맑은 고딕", 11F, FontStyle.Strikeout, GraphicsUnit.Point, 129);
+            price.ForeColor = Color.Gray;
+            price.Location = new Point(30, 158);
+            price.Name = "price";
+            price.Size = new Size(0, 30);
+            price.TabIndex = 2;
+            // 
+            // name
+            // 
+            name.AutoSize = true;
+            name.Font = new Font("맑은 고딕", 16F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            name.Location = new Point(30, 66);
+            name.Name = "name";
+            name.Size = new Size(0, 45);
+            name.TabIndex = 1;
+            // 
+            // brand
+            // 
+            brand.AutoSize = true;
+            brand.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            brand.Location = new Point(30, 19);
+            brand.Name = "brand";
+            brand.Size = new Size(0, 30);
+            brand.TabIndex = 0;
+            // 
+            // SearchDetailInputForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(1470, 1000);
+            Controls.Add(panel7);
+            Controls.Add(label4);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(2);
+            Name = "SearchDetailInputForm";
+            Text = "Form1";
+            Load += SearchDetailInputForm_Load;
+            panel5.ResumeLayout(false);
+            panel16.ResumeLayout(false);
+            panel16.PerformLayout();
+            panel14.ResumeLayout(false);
+            panel14.PerformLayout();
+            panel15.ResumeLayout(false);
+            panel15.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)img).EndInit();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Panel panel5;
+        private Label label4;
+        private Panel panel4;
+        private Panel panel7;
+        private Panel panel1;
+        private Panel panel13;
+        private Panel panel12;
+        private Panel panel11;
+        private Panel panel10;
+        private Panel panel9;
+        private Panel panel3;
+        private PictureBox pictureBox1;
+        private Label goodsDiscountPrice;
+        private Label goodsPrice;
+        private Label goodsName;
+        private Label brandNameKr;
+        private Label brand;
+        private Label name;
+        private Label price;
+        private Label discountPrice;
+        private PictureBox pictureBox2;
+        private Panel panel14;
+        private Label label6;
+        private Panel panel15;
+        private Label label8;
+        private Panel panel16;
+        private Label label9;
+        private PictureBox pictureBox4;
+        private Panel panel6;
+        private Panel panel8;
+        private PictureBox img;
+        private ComboBox comboBox1;
+        private Label label10;
+        private Label label7;
+        private Label label11;
+    }
+}
