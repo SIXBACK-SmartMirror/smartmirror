d6b7c74fabdca18a5a9380c818cfe3f61527f2d9
Kim Areum <areumk163@gmail.com>
2024-09-26 10:17:41 +0900

    Fix-BE: CaleCase 적용 (#23) Former-commit-id: acc9b2007d20ecfb14082be689f43b84272f200e

--- backend/src/main/java/com/sixback/backend/common/dto/nlp/NLPProductExtractionDto.java
+++ backend/src/main/java/com/sixback/backend/common/dto/nlp/NLPProductExtractionDto.java
@@ -12,14 +12,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class NLPProductExtractionDto {
-	//사용할 모델의 이름
+	// 사용할 모델의 이름
 	private String model;
 	// 전달할 메시지 목록(ChatMessageDto 객체로 표현)
 	private List<ChatMessageDto> messages;
 	// 생성되는 응답의 다양성을 조절하는 온도 값(기본값은 0.8)
 	private float temperature = 0.8f;
 	// 생성할 수 있는 최대 토큰 수(기본값은 35)
-	private short max_completion_tokens= 120;
+	private short maxCompletionTokens= 120;
 
 	/**
 	 * 객체를 JSON 문자열로 변환하여 반환.
@@ -29,6 +29,6 @@ public class NLPProductExtractionDto {
 	@Override
 	public String toString() {
 		return "{\"model\":\"%s\", \"messages\":%s, \"temperature\":%f, \"max_completion_tokens\":%d}"
-			.formatted(model, messages, temperature, max_completion_tokens);
+			.formatted(model, messages, temperature, maxCompletionTokens);
 	}
 }
