6e934a556ad6eda9cdbd1ca71823609c024412a4
신혜민 <alddlv81@gmail.com>
2024-09-30 13:43:48 +0900

    Feat-FE: 도면 기능 추가 (#20)
    Former-commit-id: b7ec9fb0fb5f122de1830bdfbf5a9afc97697758

--- frontend/SmartMirror/SearchDetailOutputForm.Designer.cs
+++ frontend/SmartMirror/SearchDetailOutputForm.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchInfoOutputForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchDetailOutputForm));
             title = new Label();
             panel1 = new Panel();
             label1 = new Label();
             pictureBox2 = new PictureBox();
+            panel2 = new Panel();
+            L = new Panel();
+            label13 = new Label();
+            J = new Panel();
+            label11 = new Label();
+            K = new Panel();
+            label12 = new Label();
+            H = new Panel();
+            label9 = new Label();
+            I = new Panel();
+            label10 = new Label();
+            F = new Panel();
+            label7 = new Label();
+            G = new Panel();
+            label8 = new Label();
+            B = new Panel();
+            label3 = new Label();
+            E = new Panel();
+            label6 = new Label();
+            A = new Panel();
+            label2 = new Label();
+            D = new Panel();
+            label5 = new Label();
+            C = new Panel();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
+            L.SuspendLayout();
+            J.SuspendLayout();
+            K.SuspendLayout();
+            H.SuspendLayout();
+            I.SuspendLayout();
+            F.SuspendLayout();
+            G.SuspendLayout();
+            B.SuspendLayout();
+            E.SuspendLayout();
+            A.SuspendLayout();
+            D.SuspendLayout();
+            C.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // title
@@ -57,7 +97,7 @@
             panel1.Controls.Add(label1);
             panel1.Controls.Add(pictureBox2);
             panel1.Location = new Point(109, 164);
-            panel1.Margin = new Padding(2, 2, 2, 2);
+            panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
             panel1.Size = new Size(928, 110);
             panel1.TabIndex = 2;
@@ -74,32 +114,329 @@
             // 
             // pictureBox2
             // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
             pictureBox2.Location = new Point(835, 19);
-            pictureBox2.Margin = new Padding(2, 2, 2, 2);
+            pictureBox2.Margin = new Padding(2);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(68, 68);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 0;
             pictureBox2.TabStop = false;
             // 
-            // SearchInfoOutputForm
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top;
+            panel2.Controls.Add(L);
+            panel2.Controls.Add(J);
+            panel2.Controls.Add(K);
+            panel2.Controls.Add(H);
+            panel2.Controls.Add(I);
+            panel2.Controls.Add(F);
+            panel2.Controls.Add(G);
+            panel2.Controls.Add(B);
+            panel2.Controls.Add(E);
+            panel2.Controls.Add(A);
+            panel2.Controls.Add(D);
+            panel2.Controls.Add(C);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(31, 577);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1130, 831);
+            panel2.TabIndex = 3;
+            // 
+            // L
+            // 
+            L.BackColor = Color.FromArgb(231, 231, 231);
+            L.Controls.Add(label13);
+            L.Location = new Point(635, 615);
+            L.Name = "L";
+            L.Size = new Size(210, 61);
+            L.TabIndex = 6;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label13.ForeColor = Color.FromArgb(127, 127, 127);
+            label13.Location = new Point(95, 16);
+            label13.Name = "label13";
+            label13.Size = new Size(24, 30);
+            label13.TabIndex = 5;
+            label13.Text = "L";
+            // 
+            // J
+            // 
+            J.BackColor = Color.FromArgb(231, 231, 231);
+            J.Controls.Add(label11);
+            J.Location = new Point(306, 615);
+            J.Name = "J";
+            J.Size = new Size(210, 61);
+            J.TabIndex = 6;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label11.ForeColor = Color.FromArgb(127, 127, 127);
+            label11.Location = new Point(89, 16);
+            label11.Name = "label11";
+            label11.Size = new Size(21, 30);
+            label11.TabIndex = 4;
+            label11.Text = "J";
+            // 
+            // K
+            // 
+            K.BackColor = Color.FromArgb(231, 231, 231);
+            K.Controls.Add(label12);
+            K.Location = new Point(635, 549);
+            K.Name = "K";
+            K.Size = new Size(210, 61);
+            K.TabIndex = 7;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label12.ForeColor = Color.FromArgb(127, 127, 127);
+            label12.Location = new Point(95, 16);
+            label12.Name = "label12";
+            label12.Size = new Size(26, 30);
+            label12.TabIndex = 4;
+            label12.Text = "K";
+            // 
+            // H
+            // 
+            H.BackColor = Color.FromArgb(231, 231, 231);
+            H.Controls.Add(label9);
+            H.Location = new Point(635, 430);
+            H.Name = "H";
+            H.Size = new Size(210, 61);
+            H.TabIndex = 6;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label9.ForeColor = Color.FromArgb(127, 127, 127);
+            label9.Location = new Point(94, 14);
+            label9.Name = "label9";
+            label9.Size = new Size(29, 30);
+            label9.TabIndex = 4;
+            label9.Text = "H";
+            // 
+            // I
+            // 
+            I.BackColor = Color.FromArgb(231, 231, 231);
+            I.Controls.Add(label10);
+            I.Location = new Point(306, 549);
+            I.Name = "I";
+            I.Size = new Size(210, 61);
+            I.TabIndex = 7;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label10.ForeColor = Color.FromArgb(127, 127, 127);
+            label10.Location = new Point(91, 16);
+            label10.Name = "label10";
+            label10.Size = new Size(19, 30);
+            label10.TabIndex = 3;
+            label10.Text = "I";
+            // 
+            // F
+            // 
+            F.BackColor = Color.FromArgb(231, 231, 231);
+            F.Controls.Add(label7);
+            F.Location = new Point(306, 430);
+            F.Name = "F";
+            F.Size = new Size(210, 61);
+            F.TabIndex = 6;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label7.ForeColor = Color.FromArgb(127, 127, 127);
+            label7.Location = new Point(91, 14);
+            label7.Name = "label7";
+            label7.Size = new Size(24, 30);
+            label7.TabIndex = 2;
+            label7.Text = "F";
+            // 
+            // G
+            // 
+            G.BackColor = Color.FromArgb(231, 231, 231);
+            G.Controls.Add(label8);
+            G.Location = new Point(635, 364);
+            G.Name = "G";
+            G.Size = new Size(210, 61);
+            G.TabIndex = 7;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label8.ForeColor = Color.FromArgb(127, 127, 127);
+            label8.Location = new Point(95, 16);
+            label8.Name = "label8";
+            label8.Size = new Size(28, 30);
+            label8.TabIndex = 3;
+            label8.Text = "G";
+            // 
+            // B
+            // 
+            B.BackColor = Color.FromArgb(231, 231, 231);
+            B.Controls.Add(label3);
+            B.Location = new Point(306, 247);
+            B.Name = "B";
+            B.Size = new Size(210, 61);
+            B.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label3.ForeColor = Color.FromArgb(127, 127, 127);
+            label3.Location = new Point(91, 15);
+            label3.Name = "label3";
+            label3.Size = new Size(26, 30);
+            label3.TabIndex = 1;
+            label3.Text = "B";
+            // 
+            // E
+            // 
+            E.BackColor = Color.FromArgb(231, 231, 231);
+            E.Controls.Add(label6);
+            E.Location = new Point(306, 364);
+            E.Name = "E";
+            E.Size = new Size(210, 61);
+            E.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label6.ForeColor = Color.FromArgb(127, 127, 127);
+            label6.Location = new Point(91, 16);
+            label6.Name = "label6";
+            label6.Size = new Size(24, 30);
+            label6.TabIndex = 1;
+            label6.Text = "E";
+            // 
+            // A
+            // 
+            A.BackColor = Color.FromArgb(231, 231, 231);
+            A.Controls.Add(label2);
+            A.Location = new Point(306, 181);
+            A.Name = "A";
+            A.Size = new Size(210, 61);
+            A.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label2.ForeColor = Color.FromArgb(127, 127, 127);
+            label2.Location = new Point(91, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 30);
+            label2.TabIndex = 0;
+            label2.Text = "A";
+            // 
+            // D
+            // 
+            D.BackColor = Color.FromArgb(231, 231, 231);
+            D.Controls.Add(label5);
+            D.Location = new Point(635, 247);
+            D.Name = "D";
+            D.Size = new Size(210, 61);
+            D.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label5.ForeColor = Color.FromArgb(127, 127, 127);
+            label5.Location = new Point(95, 15);
+            label5.Name = "label5";
+            label5.Size = new Size(29, 30);
+            label5.TabIndex = 2;
+            label5.Text = "D";
+            // 
+            // C
+            // 
+            C.BackColor = Color.FromArgb(231, 231, 231);
+            C.Controls.Add(label4);
+            C.Location = new Point(635, 181);
+            C.Name = "C";
+            C.Size = new Size(210, 61);
+            C.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("맑은 고딕", 11F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label4.ForeColor = Color.FromArgb(127, 127, 127);
+            label4.Location = new Point(95, 15);
+            label4.Name = "label4";
+            label4.Size = new Size(27, 30);
+            label4.TabIndex = 1;
+            label4.Text = "C";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(62, 76);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(981, 676);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // SearchDetailOutputForm
             // 
             AutoScaleDimensions = new SizeF(144F, 144F);
             AutoScaleMode = AutoScaleMode.Dpi;
             AutoSize = true;
             BackColor = SystemColors.ActiveCaptionText;
             ClientSize = new Size(1200, 1920);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(title);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(2, 2, 2, 2);
-            Name = "SearchInfoOutputForm";
+            Margin = new Padding(2);
+            Name = "SearchDetailOutputForm";
             Text = "Form1";
             WindowState = FormWindowState.Maximized;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
+            L.ResumeLayout(false);
+            L.PerformLayout();
+            J.ResumeLayout(false);
+            J.PerformLayout();
+            K.ResumeLayout(false);
+            K.PerformLayout();
+            H.ResumeLayout(false);
+            H.PerformLayout();
+            I.ResumeLayout(false);
+            I.PerformLayout();
+            F.ResumeLayout(false);
+            F.PerformLayout();
+            G.ResumeLayout(false);
+            G.PerformLayout();
+            B.ResumeLayout(false);
+            B.PerformLayout();
+            E.ResumeLayout(false);
+            E.PerformLayout();
+            A.ResumeLayout(false);
+            A.PerformLayout();
+            D.ResumeLayout(false);
+            D.PerformLayout();
+            C.ResumeLayout(false);
+            C.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -109,5 +446,31 @@
         private Panel panel1;
         private PictureBox pictureBox2;
         private Label label1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Panel D;
+        private Panel C;
+        private Panel L;
+        private Panel J;
+        private Panel K;
+        private Panel H;
+        private Panel I;
+        private Panel F;
+        private Panel G;
+        private Panel B;
+        private Panel E;
+        private Panel A;
+        private Label label2;
+        private Label label3;
+        private Label label5;
+        private Label label4;
+        private Label label9;
+        private Label label7;
+        private Label label8;
+        private Label label6;
+        private Label label10;
+        private Label label13;
+        private Label label11;
+        private Label label12;
     }
 }
--- frontend/SmartMirror/SearchDetailOutputForm.cs
+++ frontend/SmartMirror/SearchDetailOutputForm.cs
@@ -1,20 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Newtonsoft.Json.Linq;
 
 namespace SmartMirror
 {
     public partial class SearchDetailOutputForm : Form
     {
-        public SearchDetailOutputForm()
+
+        public SearchDetailOutputForm(string responseData)
         {
             InitializeComponent();
+            HighlightPanelsBasedOnLocation(responseData);
+        }
+
+        // locationList 데이터를 받아 패널을 업데이트하는 메서드
+        private void HighlightPanelsBasedOnLocation(string jsonResponse)
+        {
+            try
+            {
+                // JSON 데이터를 파싱하여 locationList 가져오기
+                JObject json = JObject.Parse(jsonResponse);
+                var locationList = json["data"]["locationList"];
+
+                // locationList를 순회하면서 패널의 색상과 텍스트를 업데이트
+                foreach (var locationData in locationList)
+                {
+                    // location 객체에서 name 값 가져오기
+                    string locationName = locationData["location"]["name"].ToString();
+                    bool isInMarket = (bool)locationData["isInMarket"];
+                    int stock = (int)locationData["stock"];
+
+                    // 패널 이름으로 기존 패널 찾기
+                    Panel panel = FindPanelByName(locationName);
+
+                    if (panel != null)
+                    {
+                        Console.WriteLine(panel.Name);
+
+                        panel.BackColor = stock > 0 ? Color.FromArgb(130, 220, 40) : Color.Yellow;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"오류 발생: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 패널 이름으로 기존 패널을 찾는 메서드
+        private Panel FindPanelByName(string panelName)
+        {
+            // Form 내의 모든 패널 중에서 이름이 일치하는 패널을 찾음
+            Panel panel = this.Controls.Find(panelName, true).FirstOrDefault() as Panel;
+            return panel;
         }
     }
 }
