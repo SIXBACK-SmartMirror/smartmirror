efc97f58becd0039e16793a40579a36afeb304e0
anchisook <songweddin@naver.com>
2024-09-30 13:29:00 +0900

    Feat-AI: 이미지 출력 rgb 조정 (#21)
    rgb  제대로 출력되게 수정
    
    
    Former-commit-id: 8e9b45b920b8f1008b95b6eb7892a3fdddf69bfa

--- ai/facer/facer_v1.py
+++ ai/facer/facer_v1.py
@@ -36,9 +36,9 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
 # 얼굴 세그멘테이션 및 색상 변경 함수
-def apply_color_changes(image_np, seg_result, eyebrow_color, skin_color, lip_color, lip_mode):
+def apply_color_changes(image_np, seg_result, eyebrowColor=None, skinColor=None, lipColor=None, lipMode="none"):
     # 피부 및 코 색상 변경
-    if skin_color is not None:
+    if skinColor is not None:
         skin_class = 1  # 피부 클래스 번호
         nose_class = 6  # 코 클래스 번호
         skin_mask = seg_result == skin_class
@@ -46,8 +46,8 @@ def apply_color_changes(image_np, seg_result, eyebrow_color, skin_color, lip_col
 
         # 기존 색과 새로운 색을 혼합 (alpha=0.8)
         alpha_face = 0.8
-        skin_color_expanded = np.tile(skin_color, (np.count_nonzero(skin_mask), 1))
-        nose_color_expanded = np.tile(skin_color, (np.count_nonzero(nose_mask), 1))
+        skin_color_expanded = np.tile(skinColor, (np.count_nonzero(skin_mask), 1))
+        nose_color_expanded = np.tile(skinColor, (np.count_nonzero(nose_mask), 1))
 
         # 피부 색상 변경
         image_np[skin_mask] = cv2.addWeighted(image_np[skin_mask].astype(np.float32), alpha_face, skin_color_expanded.astype(np.float32), 1 - alpha_face, 0)
@@ -56,7 +56,7 @@ def apply_color_changes(image_np, seg_result, eyebrow_color, skin_color, lip_col
         image_np[nose_mask] = cv2.addWeighted(image_np[nose_mask].astype(np.float32), alpha_face, nose_color_expanded.astype(np.float32), 1 - alpha_face, 0)
 
     # 눈썹 색상 변경
-    if eyebrow_color is not None:
+    if eyebrowColor is not None:
         left_eyebrow_class = 2  # 왼쪽 눈썹 클래스 번호
         right_eyebrow_class = 3  # 오른쪽 눈썹 클래스 번호
         left_eyebrow_mask = seg_result == left_eyebrow_class
@@ -64,29 +64,29 @@ def apply_color_changes(image_np, seg_result, eyebrow_color, skin_color, lip_col
 
         # 기존 색과 새로운 색을 혼합 (alpha=0.5)
         alpha = 0.5
-        eyebrow_color_expanded = np.tile(eyebrow_color, (np.count_nonzero(left_eyebrow_mask), 1))
+        eyebrow_color_expanded = np.tile(eyebrowColor, (np.count_nonzero(left_eyebrow_mask), 1))
         image_np[left_eyebrow_mask] = cv2.addWeighted(image_np[left_eyebrow_mask].astype(np.float32), alpha, eyebrow_color_expanded.astype(np.float32), 1 - alpha, 0)
         
-        eyebrow_color_expanded = np.tile(eyebrow_color, (np.count_nonzero(right_eyebrow_mask), 1))
+        eyebrow_color_expanded = np.tile(eyebrowColor, (np.count_nonzero(right_eyebrow_mask), 1))
         image_np[right_eyebrow_mask] = cv2.addWeighted(image_np[right_eyebrow_mask].astype(np.float32), alpha, eyebrow_color_expanded.astype(np.float32), 1 - alpha, 0)
 
     # 입술 색상 변경 (풀 립 또는 그라데이션)
-    if lip_color is not None:
+    if lipColor is not None:
         upper_lip_class = 7  # 윗입술 클래스 번호
         lower_lip_class = 9  # 아랫입술 클래스 번호
         upper_lip_mask = seg_result == upper_lip_class
         lower_lip_mask = seg_result == lower_lip_class
 
-        if lip_mode == "full":
+        if lipMode == "full":
             # 풀 립 색상 변경 (기존 색과 새로운 색을 혼합)
             alpha = 0.5
-            lip_color_expanded = np.tile(lip_color, (np.count_nonzero(upper_lip_mask), 1))
+            lip_color_expanded = np.tile(lipColor, (np.count_nonzero(upper_lip_mask), 1))
             image_np[upper_lip_mask] = cv2.addWeighted(image_np[upper_lip_mask].astype(np.float32), alpha, lip_color_expanded.astype(np.float32), 1 - alpha, 0)
 
-            lip_color_expanded = np.tile(lip_color, (np.count_nonzero(lower_lip_mask), 1))
+            lip_color_expanded = np.tile(lipColor, (np.count_nonzero(lower_lip_mask), 1))
             image_np[lower_lip_mask] = cv2.addWeighted(image_np[lower_lip_mask].astype(np.float32), alpha, lip_color_expanded.astype(np.float32), 1 - alpha, 0)
 
-        elif lip_mode == "gradient":
+        elif lipMode == "gradient":
             # 그라데이션 립 효과 적용
             lip_mask = upper_lip_mask | lower_lip_mask
             dist_transform = cv2.distanceTransform(lip_mask.astype(np.uint8), cv2.DIST_L2, 5)
@@ -94,12 +94,12 @@ def apply_color_changes(image_np, seg_result, eyebrow_color, skin_color, lip_col
 
             # 기존 색상과 그라데이션 마스크를 혼합하여 입술 색상 적용
             for i in range(3):  # BGR 채널
-                image_np[:, :, i] = image_np[:, :, i] * (1 - grad_mask) + lip_color[i] * grad_mask
+                image_np[:, :, i] = image_np[:, :, i] * (1 - grad_mask) + lipColor[i] * grad_mask
 
     return image_np
 
 @app.post("/ai/custom")
-async def parse_face(inputImage: UploadFile = File(...), eyebrow_color: str = "96,89,83", skin_color: str = "239,204,172", lip_color: str = "151,58,68", lip_mode: str = "full"):
+async def parse_face(inputImage: UploadFile = File(...), eyebrowColor: str = None, skinColor: str = None, lipColor: str = None, lipMode: str = "none"):
     # 이미지를 읽어서 numpy 배열로 변환
     image_bytes = await inputImage.read()
     image = Image.open(io.BytesIO(image_bytes))
@@ -129,20 +129,20 @@ async def parse_face(inputImage: UploadFile = File(...), eyebrow_color: str = "9
     seg_result = seg_probs.argmax(dim=1).squeeze().cpu().numpy()
 
     # 원본 이미지 numpy로 변환 (0~1 범위라면 0~255로 변환)
-    image_np = image_tensor.squeeze().permute(1, 2, 0).cpu().numpy()
+    image_np = image_tensor.squeeze().permute(1, 2, 0).cpu().numpy() # cpu?
     if image_np.max() <= 1:
         image_np = (image_np * 255).astype(np.uint8)
 
     # 색상 값을 처리
-    eyebrow_color = np.array([int(x) for x in eyebrow_color.split(',')], dtype=np.float32)
-    skin_color = np.array([int(x) for x in skin_color.split(',')], dtype=np.float32)
-    lip_color = np.array([int(x) for x in lip_color.split(',')], dtype=np.float32)
+    eyebrowColor = np.array([int(x) for x in eyebrowColor.split(',')], dtype=np.float32)
+    skinColor = np.array([int(x) for x in skinColor.split(',')], dtype=np.float32)
+    lipColor = np.array([int(x) for x in lipColor.split(',')], dtype=np.float32)
 
     # 색상 변경 적용
-    colored_image = apply_color_changes(image_np, seg_result, eyebrow_color, skin_color, lip_color, lip_mode)
+    colored_image = apply_color_changes(image_np, seg_result, eyebrowColor, skinColor, lipColor, lipMode)
 
     # 이미지 BGR -> RGB로 변환 및 base64로 인코딩
-    colored_image = cv2.cvtColor(colored_image.astype(np.uint8), cv2.COLOR_BGR2RGB)
+    colored_image = colored_image.astype(np.uint8)
     pil_image = Image.fromarray(colored_image)
     buffer = io.BytesIO()
     pil_image.save(buffer, format="JPEG")
