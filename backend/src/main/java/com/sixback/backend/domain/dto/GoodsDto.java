b492af1059537340599152ed1d7af628a06edf90
Kim Areum <areumk163@gmail.com>
2024-10-02 03:46:05 +0900

    Fix-BE: native query로 변경되어 interface로 수정 (#27)
    Former-commit-id: e6f380cf12fed2775ce5ec40eaa0b7502319b799

--- backend/src/main/java/com/sixback/backend/domain/dto/GoodsDto.java
+++ backend/src/main/java/com/sixback/backend/domain/dto/GoodsDto.java
@@ -1,19 +1,11 @@
 package com.sixback.backend.domain.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class GoodsDto {
-	private Long goodsId;
-	private String goodsImage;
-	private String goodsName;
-	private Long goodsPrice;
-	private Long goodsDiscountPrice;
-	private String brandNameKr;
+public interface GoodsDto {
+	Long getGoodsId();
+	String getGoodsImage();
+	String getGoodsName();
+	Long getGoodsPrice();
+	Long getGoodsDiscountPrice();
+	String getBrandNameKr();
 }
+
