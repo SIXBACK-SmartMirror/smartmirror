1494c43de3d37992af8b9ddf8c43596e60e0dde5
신혜민 <alddlv81@gmail.com>
2024-10-02 11:41:32 +0900

    Design-FE: input & output 둘 다 검색 결과 화면 보이게 수정 (#3)
    Former-commit-id: 5b35a33c4bc7942cef4acefbae0cf717a4beb680

--- frontend/SmartMirror/SearchInfoOutputForm.Designer.cs
+++ frontend/SmartMirror/SearchInfoOutputForm.Designer.cs
@@ -31,8 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SearchInfoOutputForm));
             title = new Label();
             panel1 = new Panel();
+            total = new Label();
             label1 = new Label();
             pictureBox2 = new PictureBox();
+            panel6 = new Panel();
+            label7 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
@@ -47,13 +50,14 @@
             title.Name = "title";
             title.Size = new Size(1182, 102);
             title.TabIndex = 1;
-            title.Text = "필요한 상품을 검색해 주세요";
+            title.Text = "필요한 상품을 선택해 주세요";
             title.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // panel1
             // 
             panel1.Anchor = AnchorStyles.Top;
             panel1.BackColor = SystemColors.ControlLightLight;
+            panel1.Controls.Add(total);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(pictureBox2);
             panel1.Location = new Point(109, 164);
@@ -62,6 +66,18 @@
             panel1.Size = new Size(928, 110);
             panel1.TabIndex = 2;
             panel1.Paint += panel1_Paint;
+            // 
+            // total
+            // 
+            total.Anchor = AnchorStyles.Left;
+            total.AutoSize = true;
+            total.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            total.Location = new Point(53, 31);
+            total.Margin = new Padding(2, 0, 2, 0);
+            total.Name = "total";
+            total.Size = new Size(92, 48);
+            total.TabIndex = 9;
+            total.Text = "롬앤";
             // 
             // label1
             // 
@@ -84,6 +100,28 @@
             pictureBox2.TabIndex = 0;
             pictureBox2.TabStop = false;
             // 
+            // panel6
+            // 
+            panel6.Anchor = AnchorStyles.Top;
+            panel6.AutoScroll = true;
+            panel6.BackColor = Color.Black;
+            panel6.Location = new Point(-100, 553);
+            panel6.Margin = new Padding(2);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(1501, 1603);
+            panel6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(136, 487);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 32);
+            label7.TabIndex = 8;
+            label7.Text = "총";
+            // 
             // SearchInfoOutputForm
             // 
             AutoScaleDimensions = new SizeF(144F, 144F);
@@ -91,6 +129,8 @@
             AutoSize = true;
             BackColor = SystemColors.ActiveCaptionText;
             ClientSize = new Size(1200, 1920);
+            Controls.Add(label7);
+            Controls.Add(panel6);
             Controls.Add(panel1);
             Controls.Add(title);
             FormBorderStyle = FormBorderStyle.None;
@@ -102,6 +142,7 @@
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -110,5 +151,8 @@
         private Panel panel1;
         private PictureBox pictureBox2;
         private Label label1;
+        public Panel panel6;
+        public Label label7;
+        public Label total;
     }
 }
