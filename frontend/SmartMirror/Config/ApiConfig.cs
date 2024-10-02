0073efa6fb500c420afae275be8a6f962722c40c
신혜민 <alddlv81@gmail.com>
2024-10-02 11:41:55 +0900

    Feat-FE: api 주소 설정
    Former-commit-id: ca26311164d54173898e28129cdd42e52ee658da

--- frontend/SmartMirror/Config/ApiConfig.cs
+++ frontend/SmartMirror/Config/ApiConfig.cs
@@ -2,6 +2,7 @@
 {
     public static class ApiConfig
     {
-        public const string url = "http://192.168.100.147:8080/smartMirrorApi/market";
+        public const string url = "https://j11d204.p.ssafy.io/smartMirrorApi/market";
+        //public const string url = "http://192.168.100.147:8080/smartMirrorApi/market";
     }
 }
