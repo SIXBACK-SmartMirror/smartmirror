eef6ed4557f0ad7ba73a689a3e2db2c517f57a85
Kim Areum <areumk163@gmail.com>
2024-10-04 07:29:59 +0900

    Feat-BE: 생성된 qr data DTO 구현 (#32)
    Former-commit-id: e3f6415ee46bd9a3c5fdc6dce10e7bcaf940e998

---
+++ backend/src/main/java/com/sixback/backend/domain/dto/QRDto.java
@@ -0,0 +1,16 @@
+package com.sixback.backend.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class QRDto {
+	byte [] qrImageBytes;
+}
