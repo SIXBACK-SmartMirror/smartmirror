2a0a6b2025a950be6ba3df2ebb80975d0ef9e38c
Kim Areum <areumk163@gmail.com>
2024-10-04 07:30:57 +0900

    Feat-BE: qr 요청 DTO 구현 (#32)
    Former-commit-id: 5679a0cc6e2bcc5fb03d664d9dfe24f33bba2494

---
+++ backend/src/main/java/com/sixback/backend/domain/dto/QRReqDto.java
@@ -0,0 +1,23 @@
+package com.sixback.backend.domain.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class QRReqDto {
+	private Long marketId;
+	@NotNull
+	private List<Long> optionIdList;
+	@NotNull
+	private String resultImage; // Base64
+}
