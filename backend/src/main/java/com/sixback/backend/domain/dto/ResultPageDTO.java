6b85096d2aaaf4e6431b0649ee4a6306d20bc45d
김아름 <rdf136@naver.com>
2024-10-04 07:31:38 +0900

    Feat-BE: qr 접근시 데이터 DTO 구현 (#32)
    Former-commit-id: f150385e0f077155e0622b5b25776681492dec29

---
+++ backend/src/main/java/com/sixback/backend/domain/dto/ResultPageDTO.java
@@ -0,0 +1,21 @@
+package com.sixback.backend.domain.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResultPageDTO {
+	private String marketName;
+	private List<UseOptionDetailDto> goodsList;
+	private String resultImage;
+	private String blueprintImage;
+}
