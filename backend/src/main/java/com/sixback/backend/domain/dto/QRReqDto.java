36c3f546975aa130a4190686f6d0a49eaf38e3d1
Kim Areum <areumk163@gmail.com>
2024-10-06 23:15:15 +0900

    Feat-BE: Hash 생성시 optionIdList 포함하기 위해 List용 toString 구현 (#35)
    Former-commit-id: d936b11728af1c119dd6e03b018fad6b22bb07a4

--- backend/src/main/java/com/sixback/backend/domain/dto/QRReqDto.java
+++ backend/src/main/java/com/sixback/backend/domain/dto/QRReqDto.java
@@ -1,6 +1,7 @@
 package com.sixback.backend.domain.dto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -20,4 +21,11 @@ public class QRReqDto {
 	private List<Long> optionIdList;
 	@NotNull
 	private String makeupImage; // Base64
+
+	public String getOptionIdListString(){
+		return optionIdList.stream()
+			.sorted()
+			.map(String::valueOf)
+			.collect(Collectors.joining(","));
+	}
 }
