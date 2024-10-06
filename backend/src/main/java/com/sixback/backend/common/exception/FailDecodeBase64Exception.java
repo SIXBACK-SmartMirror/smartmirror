47142eecb75df049d5e381e5d7f724dfb88f88f6
Kim Areum <areumk163@gmail.com>
2024-10-06 23:13:48 +0900

    Refactor-BE: 예외에 Exception 붙이기 (#35)
    Former-commit-id: 3fc906bfbd62abcadad8a79d8244f0f72f92a06d

--- backend/src/main/java/com/sixback/backend/common/exception/FailDecodeBase64Exception.java
+++ backend/src/main/java/com/sixback/backend/common/exception/FailDecodeBase64Exception.java
@@ -3,8 +3,8 @@ package com.sixback.backend.common.exception;
 /**
  * 빈 파일이 전달됐을 떄 예외 발생
  */
-public class FailDecodeBase64 extends CustomInputException {
-	public FailDecodeBase64 () {
+public class FailDecodeBase64Exception extends CustomInputException {
+	public FailDecodeBase64Exception() {
 		super("F03");
 	}
 }
