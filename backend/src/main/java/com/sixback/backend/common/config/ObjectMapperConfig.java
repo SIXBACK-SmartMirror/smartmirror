ff4345579820d5090b4e57727215b1fc4c4f87bb
Kim Areum <areumk163@gmail.com>
2024-10-08 10:13:31 +0900

    Comment-BE: Config 주석 추가 (#33)
    Former-commit-id: 9418a62ff8e0f541575c1782e6852f48c80f3c75

--- backend/src/main/java/com/sixback/backend/common/config/ObjectMapperConfig.java
+++ backend/src/main/java/com/sixback/backend/common/config/ObjectMapperConfig.java
@@ -7,9 +7,19 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * ObjectMapper 초기 설정 config
+ */
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ObjectMapperConfig {
+	/**
+	 *
+	 * ObjectMapper를생성하는 빈 메서드.
+	 *
+	 * @param builder Jackson 라이브러리에서 제공하는 빌더 클래스.
+	 * @return ObjectMapper XML 매퍼를 생성하지 않도록 설정하여 JSON 전용 ObjectMapper 반환.
+	 */
 	@Bean
 	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
 		return builder.createXmlMapper(false).build();
