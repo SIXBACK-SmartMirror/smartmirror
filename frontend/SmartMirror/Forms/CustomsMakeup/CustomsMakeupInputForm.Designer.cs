4d60e4d49e4da4f37591e5770e70359c48595fab
신혜민 <alddlv81@gmail.com>
2024-10-08 17:07:39 +0900

    Design-FE: 커스텀 input 디자인 수정
    Former-commit-id: 36099f224fc7f00cf38482324d3b2b050eee7ab5

--- frontend/SmartMirror/Forms/CustomsMakeup/CustomsMakeupInputForm.Designer.cs
+++ frontend/SmartMirror/Forms/CustomsMakeup/CustomsMakeupInputForm.Designer.cs
@@ -41,8 +41,6 @@
             label8 = new Label();
             panel2 = new Panel();
             label1 = new Label();
-            pictureBox1 = new PictureBox();
-            label2 = new Label();
             panel1 = new Panel();
             panel12 = new Panel();
             liplabel = new Label();
@@ -55,27 +53,33 @@
             QRpicture = new PictureBox();
             QR = new PictureBox();
             eye = new Panel();
-            makeupStart = new Panel();
-            makeupStartlabel = new Label();
+            pictureBox1 = new PictureBox();
             lip = new Panel();
             skin = new Panel();
-            panel6 = new Panel();
             clickplease = new PictureBox();
+            panel6 = new Panel();
+            panel8 = new Panel();
+            pictureBox2 = new PictureBox();
+            label5 = new Label();
+            makeupStart = new Panel();
+            label2 = new Label();
             panel5.SuspendLayout();
             panel4.SuspendLayout();
             home.SuspendLayout();
             mirror.SuspendLayout();
             panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
             panel12.SuspendLayout();
             panel9.SuspendLayout();
             panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)QRpicture).BeginInit();
             ((System.ComponentModel.ISupportInitialize)QR).BeginInit();
-            makeupStart.SuspendLayout();
-            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)clickplease).BeginInit();
+            panel6.SuspendLayout();
+            panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            makeupStart.SuspendLayout();
             SuspendLayout();
             // 
             // panel5
@@ -89,7 +93,7 @@
             panel5.Location = new Point(-2, -2);
             panel5.Margin = new Padding(2);
             panel5.Name = "panel5";
-            panel5.Size = new Size(99, 1015);
+            panel5.Size = new Size(99, 1055);
             panel5.TabIndex = 5;
             // 
             // panel4
@@ -186,28 +190,6 @@
             label1.TabIndex = 0;
             label1.Text = "메뉴";
             // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = Properties.Resources.free_sticker_makeup_11601337;
-            pictureBox1.Location = new Point(132, 35);
-            pictureBox1.Margin = new Padding(2);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(37, 35);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 2;
-            pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("맑은 고딕", 15F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            label2.Location = new Point(173, 35);
-            label2.Margin = new Padding(2, 0, 2, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(561, 41);
-            label2.TabIndex = 9;
-            label2.Text = "테스트 하고 싶은 상품을 선택해 주세요.";
-            // 
             // panel1
             // 
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
@@ -216,10 +198,10 @@
             panel1.Controls.Add(panel9);
             panel1.Controls.Add(panel3);
             panel1.ImeMode = ImeMode.NoControl;
-            panel1.Location = new Point(19, 9);
+            panel1.Location = new Point(45, 4);
             panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1323, 90);
+            panel1.Size = new Size(812, 139);
             panel1.TabIndex = 0;
             // 
             // panel12
@@ -229,61 +211,61 @@
             panel12.Location = new Point(12, 11);
             panel12.Margin = new Padding(3, 4, 3, 4);
             panel12.Name = "panel12";
-            panel12.Size = new Size(120, 70);
+            panel12.Size = new Size(217, 83);
             panel12.TabIndex = 3;
             // 
             // liplabel
             // 
             liplabel.AutoSize = true;
-            liplabel.Font = new Font("맑은 고딕", 16F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            liplabel.Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
             liplabel.ForeColor = Color.White;
-            liplabel.Location = new Point(17, 12);
+            liplabel.Location = new Point(88, 23);
             liplabel.Name = "liplabel";
-            liplabel.Size = new Size(84, 45);
+            liplabel.Size = new Size(38, 32);
             liplabel.TabIndex = 1;
-            liplabel.Text = "입술";
+            liplabel.Text = "립";
             liplabel.Click += lipListLabel_Click;
             // 
             // panel9
             // 
             panel9.BackColor = Color.WhiteSmoke;
             panel9.Controls.Add(skinListLabel);
-            panel9.Location = new Point(268, 11);
+            panel9.Location = new Point(467, 11);
             panel9.Margin = new Padding(3, 4, 3, 4);
             panel9.Name = "panel9";
-            panel9.Size = new Size(120, 70);
+            panel9.Size = new Size(217, 83);
             panel9.TabIndex = 3;
             // 
             // skinListLabel
             // 
             skinListLabel.AutoSize = true;
-            skinListLabel.Font = new Font("맑은 고딕", 16F);
-            skinListLabel.Location = new Point(17, 12);
+            skinListLabel.Font = new Font("맑은 고딕", 12F);
+            skinListLabel.Location = new Point(66, 23);
             skinListLabel.Name = "skinListLabel";
-            skinListLabel.Size = new Size(84, 45);
+            skinListLabel.Size = new Size(86, 32);
             skinListLabel.TabIndex = 1;
-            skinListLabel.Text = "피부";
+            skinListLabel.Text = "베이스";
             skinListLabel.Click += skinListLabel_Click;
             // 
             // panel3
             // 
             panel3.BackColor = Color.WhiteSmoke;
             panel3.Controls.Add(eyebrowListLabel);
-            panel3.Location = new Point(140, 11);
+            panel3.Location = new Point(240, 11);
             panel3.Margin = new Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
-            panel3.Size = new Size(120, 70);
+            panel3.Size = new Size(217, 83);
             panel3.TabIndex = 2;
             // 
             // eyebrowListLabel
             // 
             eyebrowListLabel.AutoSize = true;
-            eyebrowListLabel.Font = new Font("맑은 고딕", 16F);
-            eyebrowListLabel.Location = new Point(17, 12);
+            eyebrowListLabel.Font = new Font("맑은 고딕", 12F);
+            eyebrowListLabel.Location = new Point(46, 23);
             eyebrowListLabel.Name = "eyebrowListLabel";
-            eyebrowListLabel.Size = new Size(84, 45);
+            eyebrowListLabel.Size = new Size(134, 32);
             eyebrowListLabel.TabIndex = 1;
-            eyebrowListLabel.Text = "눈썹";
+            eyebrowListLabel.Text = "아이브로우";
             eyebrowListLabel.Click += eyebrowListLabel_Click;
             // 
             // lipListLabel
@@ -301,18 +283,18 @@
             // 
             clickList.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             clickList.AutoScroll = true;
-            clickList.BackColor = Color.White;
-            clickList.Location = new Point(19, 595);
+            clickList.BackColor = Color.WhiteSmoke;
+            clickList.Location = new Point(187, 667);
             clickList.Margin = new Padding(3, 4, 3, 4);
             clickList.Name = "clickList";
-            clickList.Size = new Size(953, 248);
+            clickList.Size = new Size(616, 246);
             clickList.TabIndex = 1;
             // 
             // QRpicture
             // 
             QRpicture.BackColor = Color.White;
             QRpicture.Image = (Image)resources.GetObject("QRpicture.Image");
-            QRpicture.Location = new Point(620, 616);
+            QRpicture.Location = new Point(829, 650);
             QRpicture.Margin = new Padding(3, 4, 3, 4);
             QRpicture.Name = "QRpicture";
             QRpicture.Size = new Size(339, 65);
@@ -325,10 +307,10 @@
             // 
             QR.BackColor = Color.White;
             QR.Image = (Image)resources.GetObject("QR.Image");
-            QR.Location = new Point(833, 674);
+            QR.Location = new Point(47, 47);
             QR.Margin = new Padding(3, 4, 3, 4);
             QR.Name = "QR";
-            QR.Size = new Size(126, 129);
+            QR.Size = new Size(73, 72);
             QR.SizeMode = PictureBoxSizeMode.Zoom;
             QR.TabIndex = 9;
             QR.TabStop = false;
@@ -339,97 +321,145 @@
             // 
             eye.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             eye.AutoScroll = true;
-            eye.BackColor = Color.White;
-            eye.Location = new Point(19, 106);
+            eye.BackColor = Color.WhiteSmoke;
+            eye.Location = new Point(53, 122);
             eye.Margin = new Padding(3, 4, 3, 4);
             eye.Name = "eye";
-            eye.Size = new Size(1323, 466);
+            eye.Size = new Size(1212, 482);
             eye.TabIndex = 3;
             eye.Visible = false;
             // 
-            // makeupStart
+            // pictureBox1
             // 
-            makeupStart.BackColor = Color.FromArgb(134, 147, 250);
-            makeupStart.Controls.Add(makeupStartlabel);
-            makeupStart.Location = new Point(997, 665);
-            makeupStart.Margin = new Padding(3, 4, 3, 4);
-            makeupStart.Name = "makeupStart";
-            makeupStart.Size = new Size(346, 85);
-            makeupStart.TabIndex = 7;
-            makeupStart.Click += makeupStart_Click;
-            // 
-            // makeupStartlabel
-            // 
-            makeupStartlabel.AutoSize = true;
-            makeupStartlabel.Location = new Point(109, 31);
-            makeupStartlabel.Name = "makeupStartlabel";
-            makeupStartlabel.Size = new Size(119, 25);
-            makeupStartlabel.TabIndex = 0;
-            makeupStartlabel.Text = "Makeup start";
-            makeupStartlabel.Click += makeupStart_Click;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(165, 182);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += makeupStart_Click;
             // 
             // lip
             // 
             lip.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             lip.AutoScroll = true;
-            lip.BackColor = Color.White;
-            lip.Location = new Point(19, 106);
+            lip.BackColor = Color.WhiteSmoke;
+            lip.Location = new Point(52, 122);
             lip.Margin = new Padding(3, 4, 3, 4);
             lip.Name = "lip";
-            lip.Size = new Size(1323, 466);
+            lip.Size = new Size(1212, 482);
             lip.TabIndex = 6;
             // 
             // skin
             // 
             skin.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             skin.AutoScroll = true;
-            skin.BackColor = Color.White;
-            skin.Location = new Point(19, 106);
+            skin.BackColor = Color.WhiteSmoke;
+            skin.Location = new Point(55, 120);
             skin.Margin = new Padding(3, 4, 3, 4);
             skin.Name = "skin";
-            skin.Size = new Size(1323, 466);
+            skin.Size = new Size(1212, 482);
             skin.TabIndex = 5;
             skin.Visible = false;
+            // 
+            // clickplease
+            // 
+            clickplease.Image = (Image)resources.GetObject("clickplease.Image");
+            clickplease.Location = new Point(829, 643);
+            clickplease.Margin = new Padding(3, 4, 3, 4);
+            clickplease.Name = "clickplease";
+            clickplease.Size = new Size(179, 72);
+            clickplease.SizeMode = PictureBoxSizeMode.Zoom;
+            clickplease.TabIndex = 8;
+            clickplease.TabStop = false;
+            clickplease.Visible = false;
             // 
             // panel6
             // 
             panel6.Anchor = AnchorStyles.None;
-            panel6.BackColor = Color.FromArgb(248, 248, 248);
+            panel6.BackColor = Color.White;
             panel6.Controls.Add(QRpicture);
-            panel6.Controls.Add(QR);
+            panel6.Controls.Add(panel8);
             panel6.Controls.Add(clickplease);
+            panel6.Controls.Add(label5);
             panel6.Controls.Add(skin);
             panel6.Controls.Add(lip);
             panel6.Controls.Add(makeupStart);
             panel6.Controls.Add(eye);
             panel6.Controls.Add(clickList);
             panel6.Controls.Add(panel1);
-            panel6.Location = new Point(101, 148);
+            panel6.Location = new Point(101, 84);
             panel6.Margin = new Padding(2);
             panel6.Name = "panel6";
-            panel6.Size = new Size(1370, 854);
+            panel6.Size = new Size(1370, 953);
             panel6.TabIndex = 6;
             // 
-            // clickplease
+            // panel8
             // 
-            clickplease.Image = (Image)resources.GetObject("clickplease.Image");
-            clickplease.Location = new Point(1059, 580);
-            clickplease.Margin = new Padding(3, 4, 3, 4);
-            clickplease.Name = "clickplease";
-            clickplease.Size = new Size(237, 80);
-            clickplease.SizeMode = PictureBoxSizeMode.Zoom;
-            clickplease.TabIndex = 8;
-            clickplease.TabStop = false;
-            clickplease.Visible = false;
+            panel8.BackColor = Color.White;
+            panel8.Controls.Add(QR);
+            panel8.Controls.Add(pictureBox2);
+            panel8.Location = new Point(1022, 719);
+            panel8.Margin = new Padding(3, 4, 3, 4);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(165, 200);
+            panel8.TabIndex = 8;
+            panel8.Click += QR_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(3, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(159, 182);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Visible = false;
+            pictureBox2.Click += QR_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.FromArgb(64, 64, 64);
+            label5.Font = new Font("맑은 고딕", 11F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(41, 667);
+            label5.Name = "label5";
+            label5.Size = new Size(131, 30);
+            label5.TabIndex = 0;
+            label5.Text = "선택한 옵션";
+            // 
+            // makeupStart
+            // 
+            makeupStart.BackColor = Color.White;
+            makeupStart.Controls.Add(pictureBox1);
+            makeupStart.Location = new Point(844, 719);
+            makeupStart.Margin = new Padding(3, 4, 3, 4);
+            makeupStart.Name = "makeupStart";
+            makeupStart.Size = new Size(172, 200);
+            makeupStart.TabIndex = 7;
+            makeupStart.Click += makeupStart_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 15F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.Location = new Point(117, 22);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(411, 41);
+            label2.TabIndex = 9;
+            label2.Text = "원하는 옵션을 선택해 주세요";
             // 
             // CustomsMakeupInputForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlLightLight;
-            ClientSize = new Size(1470, 1000);
+            ClientSize = new Size(1470, 1040);
             Controls.Add(label2);
-            Controls.Add(pictureBox1);
             Controls.Add(panel6);
             Controls.Add(panel5);
             FormBorderStyle = FormBorderStyle.None;
@@ -447,7 +477,6 @@
             mirror.PerformLayout();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel1.ResumeLayout(false);
             panel12.ResumeLayout(false);
             panel12.PerformLayout();
@@ -457,18 +486,19 @@
             panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)QRpicture).EndInit();
             ((System.ComponentModel.ISupportInitialize)QR).EndInit();
-            makeupStart.ResumeLayout(false);
-            makeupStart.PerformLayout();
-            panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)clickplease).EndInit();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            makeupStart.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
         private Panel panel5;
-        private PictureBox pictureBox1;
-        private Label label2;
         private Panel panel1;
         private Panel panel9;
         private Label skinListLabel;
@@ -477,8 +507,6 @@
         private Label lipListLabel;
         private Panel clickList;
         private Panel eye;
-        private Panel makeupStart;
-        private Label makeupStartlabel;
         private Panel lip;
         private Panel skin;
         private Panel panel6;
@@ -495,6 +523,12 @@
         private Label label1;
         private Panel panel4;
         private Label label4;
+        private Label label2;
+        private Label label5;
+        private PictureBox pictureBox1;
+        private Panel panel8;
+        private PictureBox pictureBox2;
+        private Panel makeupStart;
     }
 }
 
